687f09d9e2b6af716e9ff9cfacbcbcd275e798cd
vinothkumar <net.vinothkumar@gmail.com>
2021-11-01 21:29:08 +0100

    Creating patient insight project

---
+++ src/main/java/com/datatower/patientsInsight/PatientsInsightApplication.java
@@ -0,0 +1,13 @@
+package com.datatower.patientsInsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientsInsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientsInsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datatower/patientsInsight/PatientsInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datatower.patientsInsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientsInsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
